9d77b251a714125ca75d761c1ab335b740796290
Lesley <glei164@aucklanduni.ac.nz>
2024-10-27 23:17:23 +1300

    Update .gitignore

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -4,16 +4,14 @@ import {getToken} from "firebase/messaging";
 
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyC5RAgWKOYkm9yr9HtRiD5hOKMuHk82eKg",
-  authDomain: "cs732-happy-hippos.firebaseapp.com",
-  projectId: "cs732-happy-hippos",
-  storageBucket: "cs732-happy-hippos.appspot.com",
-  messagingSenderId: "347789660796",
-  appId: "1:347789660796:web:86a6c0c56446459b7f2ff9",
-  measurementId: "G-24NHB1YJWG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-const vapidKey="BDNL-SQPTWBKaRD2-c0G5-A9WoCewRi5VfXrFX5q9T7CYSJGCB52zGO_khLQc8v9sQ-kVi6EWMogpdfzKqK7lK0"
-
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -24,7 +22,7 @@ const messaging = firebase.messaging();
 
 // Get the device token
 async function getDeviceToken() {
-  return await getToken(messaging, {vapidKey: vapidKey});
+  return await getToken(messaging, {vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY});
 }
 
 export { messaging, getDeviceToken };
